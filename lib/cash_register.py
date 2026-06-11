c579f39b2fbe8a4e18908e38afe8d40cce446789
Jean <jean.koech@student.moringaschool.com>
2026-06-11 18:48:17 +0300

    Update latest changes

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-    def __init__(self):
+    def __init__(self, discount=0):
         self.total = 0
-        self.discount = 0
+        self.discount = discount
         self.items = []
         self.previous_transactions = []
 
@@ -15,12 +15,12 @@ class CashRegister:
             self.items.append(item)
 
     def apply_discount(self):
-        if self.total > 3000:
+        if self.discount:
             discount_amount = (self.discount / 100) * self.total
             self.total -= discount_amount
-            return f"After the discount, the total comes to kshs {int(self.total)}."
+            print(f"After the discount, the total comes to ${int(self.total)}.")
         else:
-            return "There is no discount to apply."
+            print("There is no discount to apply.")
         
 
     def void_last_transaction(self):
@@ -28,25 +28,26 @@ class CashRegister:
             last_transaction = self.previous_transactions.pop()
             self.total -= last_transaction
 
-my_cash_register = CashRegister()
-my_cash_register.discount = 20
+if __name__ == "__main__":
+    my_cash_register = CashRegister()
+    my_cash_register.discount = 20
 
-my_cash_register.add_item("milk", 200.00, 2)
-print(my_cash_register.total)  # Output: 400.0
+    my_cash_register.add_item("milk", 200.00, 2)
+    print(my_cash_register.total)  # Output: 400.0
 
-my_cash_register.add_item("loaf of bread", 100.50, 1)
-print(my_cash_register.total)  # Output: 500.5
+    my_cash_register.add_item("loaf of bread", 100.50, 1)
+    print(my_cash_register.total)  # Output: 500.5
 
-my_cash_register.add_item("sausages", 335.00)
-print(my_cash_register.total)  # Output: 835.5
+    my_cash_register.add_item("sausages", 335.00)
+    print(my_cash_register.total)  # Output: 835.5
 
-my_cash_register.add_item("cake", 900.0, 2)
-print(my_cash_register.total)  # Output: 2635.5
+    my_cash_register.add_item("cake", 900.0, 2)
+    print(my_cash_register.total)  # Output: 2635.5
 
-my_cash_register.add_item("biscuit", 300.0, 2)
-print(my_cash_register.total)  # Output: 3235.5
+    my_cash_register.add_item("biscuit", 300.0, 2)
+    print(my_cash_register.total)  # Output: 3235.5
 
-my_cash_register.add_item("Ribena juice", 275.00, 5)
-print(my_cash_register.total)  # Output: 4610.5
+    my_cash_register.add_item("Ribena juice", 275.00, 5)
+    print(my_cash_register.total)  # Output: 4610.5
 
-print(my_cash_register.apply_discount())  # Output: "After the discount, the total comes to kshs 3688."
+    my_cash_register.apply_discount()  # Output printed: "After the discount, the total comes to $3688."
